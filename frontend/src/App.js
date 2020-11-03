8f8d1429ea6d0f00f6f6941a610d03cf2032adad
Vitalij202020 <vaskouski.v.v@gmail.com>
2020-11-03 20:18:38 +0300

    added folder image plus file with data and added component product also added folder screens with HomeScreen file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Footer from './components/Footer'
 import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import './bootstrap.min.css'
+import HomeScrren from './screens/HomeScrren'
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <Header />
       <main>
         <Container>
-          <h1>Welcome To ProShop</h1>
+          <HomeScrren />
         </Container>
       </main>
       <Footer />
