abd72e72558306a226c048d308a2236506220992
Vitalij202020 <vaskouski.v.v@gmail.com>
2020-11-03 18:28:30 +0300

    added header and footer component also added bootstrap and font-awesome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Footer from './components/Footer'
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import './bootstrap.min.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
