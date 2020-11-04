2f86969f62abedd40542b18b0aad33529a845523
Vitalij202020 <vaskouski.v.v@gmail.com>
2020-11-04 15:59:44 +0300

    added react-router-dom and react-router-bootstarap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Footer from './components/Footer'
 import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import './bootstrap.min.css'
-import HomeScrren from './screens/HomeScrren'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <main>
-        <Container>
-          <HomeScrren />
-        </Container>
-      </main>
-      <Footer />
-    </>
+    <Router>
+      <>
+        <Header />
+        <main>
+          <Container>
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/product" component={ProductScreen} />
+          </Container>
+        </main>
+        <Footer />
+      </>
+    </Router>
   )
 }
 
